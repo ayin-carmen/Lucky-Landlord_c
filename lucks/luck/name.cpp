e88e044fed76f7c2ad543c6476efa2e01df6ebd4
ayin-carmen <2112160741@qq.com>
2024-04-03 12:32:35 +0800

    test

---
+++ lucks/luck/name.cpp
@@ -0,0 +1,30 @@
+#include "statement.h"
+#include <iostream>
+#include <vector>
+using namespace  std;
+int main()
+{
+	int rent = 0;//房租,即每一次要交的钱
+
+	int playMoney = 0;//玩家所持有的钱
+
+	vector<int>commonItems;//游戏内所有普通物品
+
+	vector<int>specialItems;//游戏内所有道具
+
+	vector<int>primeItems;//游戏内所有的精华
+
+	vector<int>gamblingMachinesItems;//老虎机上的20个物品
+
+	vector<int>playCommonItems;//玩家目前持有的物品
+
+	vector<int>playSpecialItem;//玩家目前持有的道具
+
+	vector<int>playPrimeItem;//玩家目前持有的精华
+
+	int deleteItem = 0;//可删除的次数
+
+	int refreshItem = 0;//可刷新的次数
+
+	int countDay = 0;//记录天数
+}
