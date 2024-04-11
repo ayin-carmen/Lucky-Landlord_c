63c59b82696315572f51007e243392d6414d523d
Dlicy <146825414+Dlicy@users.noreply.github.com>
2024-04-11 17:24:04 +0800

    test

---
+++ luckyhome/luckyhome/Monkey.h
@@ -0,0 +1,24 @@
+#ifndef MONKEY_H
+#define MONKEY_H
+
+#include <iostream>
+#include <string>
+#include "../luckyhome/Thing.h"
+#include "../luckyhome/Coconut.h"
+#include "../luckyhome/Halfcoconut.h"
+#include <vector>
+
+class Monkey : public Thing {
+private:
+    int position;
+
+public:
+    Monkey();
+
+    int calculateMoney(std::vector<Thing*>* category, std::vector<Props*>* playerScene, std::vector<Thing*>* playerItem) override;
+    Monkey* createNewItem() override;
+    //void setPosition(int newPosition);
+    bool isNear(int position1, int position2) override;
+};
+
+#endif // MONKEY_H
